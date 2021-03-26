c261af57b9dd08df6123059e73644b53e1d9b689
bbeuro132 <81130257+bbeuro132@users.noreply.github.com>
2021-03-26 10:36:35 +0900

    C# 2차원 배열 공부
    각 행마다 다른 개수의 열을 삽입할 수 있음을 알았다.

---
+++ 2차원배열 공부.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ClassProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] a =
+            {
+                new int[3],
+                new int[5]
+            };
+            
+            for (int show = 0; show < a.Length; show++)
+            {
+                for (int show2 = 0; show2 < a[show].Length; show2++)
+                {
+                    Console.Write((show + 1) + "번줄\t");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
